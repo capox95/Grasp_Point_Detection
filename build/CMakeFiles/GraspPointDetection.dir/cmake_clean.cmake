ec3ed0cfea07321d660697d62297fb53fe812d9c
capox95 <capox95@gmail.com>
2019-10-02 12:26:15 +0200

    first commit

---
+++ build/CMakeFiles/GraspPointDetection.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraspPointDetection.dir/src/main.cpp.o"
+  "CMakeFiles/GraspPointDetection.dir/src/entropy.cpp.o"
+  "CMakeFiles/GraspPointDetection.dir/src/binsegmentation.cpp.o"
+  "CMakeFiles/GraspPointDetection.dir/src/pointpose.cpp.o"
+  "GraspPointDetection.pdb"
+  "GraspPointDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraspPointDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,76 @@
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+#include <chrono>
+#include <ctime>
+
+#include "../include/entropy.h"
+#include "../include/binsegmentation.h"
+#include "../include/pointpose.h"
+
+//----------------------------------------------------------------------------- //
+int main(int argc, char **argv)
+{
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr source(new pcl::PointCloud<pcl::PointXYZRGB>);
+    if (pcl::io::loadPCDFile<pcl::PointXYZRGB>(argv[1], *source) == -1)
+    {
+        PCL_ERROR(" error opening file ");
+        return (-1);
+    }
+    std::cout << "cloud orginal size: " << source->size() << std::endl;
+
+    //time computation
+    auto start = std::chrono::steady_clock::now();
+    //BIN SEGMENTATION -----------------------------------------------------------------------
+
+    BinSegmentation bin;
+    bin.setInputCloud(source);
+    bin.setNumberLines(4);
+    bin.setScaleFactorHullBorders(0.2);
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_grasp(new pcl::PointCloud<pcl::PointXYZRGB>);
+    bin.compute(cloud_grasp);
+
+    pcl::ModelCoefficients::Ptr plane = bin.getPlaneGroundPoints();
+
+    //time computation
+    auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
+    std::cout << "duration segmentation: " << std::chrono::duration<double, std::milli>(diff).count() << " ms" << std::endl;
+    std::cout << std::endl;
+
+    auto startE = std::chrono::steady_clock::now();
+    // ENTROPY FILTER -----------------------------------------------------------------------
+    //
+    EntropyFilter ef;
+    ef.setInputCloud(cloud_grasp);
+    ef.setDownsampleLeafSize(0.005);
+    ef.setEntropyThreshold(0.7);
+    ef.setKLocalSearch(500);        // Nearest Neighbour Local Search
+    ef.setCurvatureThreshold(0.01); //Curvature Threshold for the computation of Entropy
+    ef.setDepthThreshold(0.03);
+    ef.setAngleThresholdForConvexity(5);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_result(new pcl::PointCloud<pcl::PointXYZ>);
+    ef.compute(cloud_result);
+
+    // GRASP POINT --------------------------------------------------------------------------
+    PointPose pp;
+    pp.setSourceCloud(source);
+    pp.setRefPlane(plane);
+    pp.setInputCloud(cloud_result);
+    pp.computeGraspPoint();
+
+    //time computation
+    auto endE = std::chrono::steady_clock::now();
+    auto diff2 = endE - startE;
+    std::cout << "duration entropy filter: " << std::chrono::duration<double, std::milli>(diff2).count() << " ms" << std::endl;
+
+    //bin.visualizeNoSpin(true, true);
+    //pp.visualizeGrasp();
+    //ef.visualizeAll(false);
+
+    return 0;
+}
